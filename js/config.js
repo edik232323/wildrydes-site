43bb26b0b5dac83e5eac7806280903de20926cb5
edik232323 <144430925+edik232323@users.noreply.github.com>
2024-11-25 21:39:06 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_fn4SquQiv', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6r30lo60s1dcerbk5muqdrn08k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' /eu-north-1/ e.g. us-east-2
+        userPoolId: 'eu-north-1_EfSJOFFl0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '24e070gtd10658e936obg0apfv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' /eu-north-1/ e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
