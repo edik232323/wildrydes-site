446430acced6c031d70094edb7a9ff2b6caaeda7
edik232323 <144430925+edik232323@users.noreply.github.com>
2024-11-26 12:45:17 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_EfSJOFFl0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '24e070gtd10658e936obg0apfv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_gMnXK0qKe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '11vr92regaamipddjkmv7ad9t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
